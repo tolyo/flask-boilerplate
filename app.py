2034d2926f2cce84395e31990485f602a7ad9efb
tolyo <anatolyostrovsky@gmail.com>
2023-09-15 03:16:45 +0300

    Sample templates

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(
+    __name__, 
+    template_folder='app',
+    static_folder='app'
+)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/user')
+def user():
+    return render_template('user/user.html')
+
+if __name__ == '__main__':
+    app.run()
